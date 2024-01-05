d8a4966d58623450c83923ce4160ab2eca536396
TSAELENS <77736077+TSAELENS@users.noreply.github.com>
2024-01-05 12:37:33 +0100

    Fix start

---
+++ src/controllers/orderController.js
@@ -0,0 +1,20 @@
+exports.getAllOrders = (req, res) => {
+    // Logique pour retourner tous les ordres
+  };
+  
+  exports.getOrderById = (req, res) => {
+    // Logique pour retourner un ordre spécifique par ID
+  };
+  
+  exports.createOrder = (req, res) => {
+    // Logique pour créer un nouvel ordre
+  };
+  
+  exports.updateOrder = (req, res) => {
+    // Logique pour mettre à jour un ordre par ID
+  };
+  
+  exports.deleteOrder = (req, res) => {
+    // Logique pour supprimer un ordre par ID
+  };
+  
